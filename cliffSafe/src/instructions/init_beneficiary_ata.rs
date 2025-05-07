d99b30bc51a22810a84f4a0e384b4eae0a29dbe5
Mohammed Zeeshan <93642091+alizeeshan1234@users.noreply.github.com>
2025-05-07 23:45:20 +0530

    Update init_beneficiary_ata.rs

--- cliffSafe/src/instructions/init_beneficiary_ata.rs
+++ cliffSafe/src/instructions/init_beneficiary_ata.rs
@@ -3,33 +3,30 @@ use anchor_spl::token::*;
 use anchor_spl::associated_token::AssociatedToken;
 
 #[derive(Accounts)]
-pub struct InitializeBeneficiaryATA<'info> {
+pub struct InitBeneficiaryAta<'info> {
     #[account(mut)]
     pub beneficiary: Signer<'info>,
 
-    #[account(
-        mint::token_program = token_program,
-    )]
     pub mint: Account<'info, Mint>,
 
     #[account(
-        init_if_needed,
+        init,
         payer = beneficiary,
         associated_token::mint = mint,
         associated_token::authority = beneficiary,
     )]
-    pub beneficiary_ata: Account<'info, TokenAccount>,
-
-    pub token_program: Program<'info, Token>,
+    pub beneficiary_ata: Box<Account<'info, TokenAccount>>,
 
     pub system_program: Program<'info, System>,
+
+    pub token_program: Program<'info, Token>,
 
     pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
-impl<'info> InitializeBeneficiaryATA<'info> {
-    pub fn create_beneficiary_ata(&mut self) -> Result<()> {
-        msg!("Beneficiary ATA initialized successfully");
+impl<'info> InitBeneficiaryAta<'info> {
+    pub fn init_beneficiary_ata(&mut self) -> Result<()> {
+        msg!("Beneficiary ATA initialized");
         Ok(())
     }
 }
